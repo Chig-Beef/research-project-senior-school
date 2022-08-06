7b7006390ede30397105d9768b4a51d96175b6f3
Chig Beef <102199398+Chig-Beef@users.noreply.github.com>
2022-08-06 10:28:29 +0930

    Added...
    Icon to pages
    Side bar and main section are now side by side
    Added buttons that take you to different pages

---
+++ script.js
@@ -0,0 +1,4 @@
+btnHome.onclick = function (){window.open('index.html');window.close();};
+btnTeachers.onclick = function (){window.open('teachers.html');window.close();};
+btnClassrooms.onclick = function (){window.open('classrooms.html');window.close();};
+btnSubjects.onclick = function (){window.open('subjects.html');window.close();};
